2d7a2652f9b6b6d1fdf5278a89e33e640f77c6d3
Brian <brian@Brians-MacBook-Pro.local>
2023-02-07 23:00:33 +0000

    static files fix

--- capstone/settings.py
+++ capstone/settings.py
@@ -147,10 +147,9 @@ DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
-
+STATIC_URL = 'static/'
 
 # Static file serving.
 # http://whitenoise.evans.io/en/stable/django.html#django-middleware
